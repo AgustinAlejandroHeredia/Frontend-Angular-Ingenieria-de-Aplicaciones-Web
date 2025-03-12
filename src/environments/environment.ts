1b118c09410288469f392959c5e8bf5ad35f4494
AgustinAlejandroHeredia <agushcorreo@gmail.com>
2025-03-12 11:55:36 -0300

    Primera correccion - falta de archivo enviroment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    auth0Domain: 'dev-s2w5lzkgxmbf0wl0.us.auth0.com',
+    auth0ClientId: 'S1QqgeE9Tu27X8RYsxFnhtshIeMDZIte',
+    auth0Secret: 'RXbEsN_TGCp9S2FnctxTdr5ro6olbsLp-EJYuArSVTyFHUp1b_J_gWjnodEVyLZ3',
+    audience: 'https://inge.com/api/orders',
+    apiUrl: 'http://localhost:3000',
+}
+
+export const apiUrl = environment.apiUrl;
